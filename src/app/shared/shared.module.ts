f7945f3ccb0c8eaa252e3232f7a4e386628c4f9d
MarcosWinther <winthermarcos@gmail.com>
2023-10-05 12:58:59 -0300

    Feito a parte de listagem dos Pokemons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
 import { PokeListComponent } from './poke-list/poke-list.component';
-
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
     PokeListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
